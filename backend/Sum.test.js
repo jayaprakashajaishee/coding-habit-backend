42f87fb25a7095719041731a1d1a24bc56857ec3
jayaprakashajaishee <jayaprakashajaishee@gmail.com>
2022-04-11 16:45:53 +0530

    5 files added

---
+++ backend/Sum.test.js
@@ -0,0 +1,13 @@
+const Sum = require('./Sum.cjs');
+
+test('Adds two numbers', () => {
+  expect(Sum(1, 2)).toBe(3);
+});
+
+test('Returns a number ', () => {
+  expect(typeof Sum(1, 2)).toBe('number');
+});
+
+test('Works with negative numbers', () => {
+  expect(Sum(2, -4)).toBe(-2);
+});
---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import { exec } from 'child_process';
+
+const PORT = 4200;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+app.post('/run', (req, res) => {
+  const { command } = req.body;
+
+  console.log(`[INFO] Running "${command}"`);
+
+  const child = exec(command);
+
+  child.stdout.setEncoding('utf8');
+  child.stderr.setEncoding('utf8');
+
+  let stdout = '';
+  let stderr = '';
+
+  child.stdout.on('data', (data) => {
+    stdout += data;
+  });
+
+  child.stderr.on('data', (data) => {
+    stderr += data;
+  });
+
+  child.on('exit', () => {
+    res.send({ stdout, stderr });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: http://localhost:${PORT}`);
+});
